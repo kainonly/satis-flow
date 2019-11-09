681a98305ae0abc1081fb02151f86d7468216945
kain <zhangtqx@vip.qq.com>
2019-11-09 14:28:46 +0800

    Submit

--- main.py
+++ main.py
@@ -5,35 +5,41 @@ import docker
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-client = docker.from_env()
-client.login(
-    username=config['docker']['username'],
-    password=config['docker']['password'],
-    registry=config['docker']['registry']
-)
-repository = config['docker']['repository']
-image = client.images.get(repository)
-if image is None:
-    client.images.pull(repository)
+try:
+    client = docker.DockerClient(
+        base_url=config['docker']['base_url']
+    )
+    client.login(
+        username=config['docker']['username'],
+        password=config['docker']['password'],
+        registry=config['docker']['registry']
+    )
+    repository = config['docker']['repository']
+    image = client.images.get(repository)
+    if image is None:
+        client.images.pull(repository)
 
-result = client.containers.run(
-    image=repository,
-    command='build ./satis.json ./public',
-    init=True,
-    auto_remove=True,
-    volumes={
-        os.path.abspath('./factory/satis.json'): {
-            'bind': '/build/satis.json',
-            'mode': 'ro'
+    result = client.containers.run(
+        image=repository,
+        command='build ./satis.json ./public',
+        init=True,
+        auto_remove=True,
+        volumes={
+            os.path.abspath('./factory/satis.json'): {
+                'bind': '/build/satis.json',
+                'mode': 'ro'
+            },
+            os.path.abspath('./factory/composer'): {
+                'bind': '/composer',
+                'mode': 'rw'
+            },
+            os.path.abspath('./factory/public'): {
+                'bind': '/build/public',
+                'mode': 'rw'
+            }
         },
-        os.path.abspath('./factory/composer'): {
-            'bind': '/composer',
-            'mode': 'rw'
-        },
-        os.path.abspath('./factory/public'): {
-            'bind': '/build/public',
-            'mode': 'rw'
-        }
-    },
-    working_dir='/build'
-)
+        working_dir='/build'
+    )
+
+except Exception as message:
+    print(message)
